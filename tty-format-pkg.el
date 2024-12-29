7360f9e1c28a5e073dcae938a8ea66619c058f6b
bzadonn <177057655+bzadon@users.noreply.github.com>
2024-12-29 12:28:16 -0500

    version 12

---
+++ tty-format-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from tty-format.el  -*- no-byte-compile: t -*-
+(define-package "tty-format" "12" "text file backspacing and ANSI SGR as faces" 'nil :authors '(("Kevin Ryde" . "user42_kevin@yahoo.com.au")) :maintainer '("Kevin Ryde" . "user42_kevin@yahoo.com.au") :url "http://user42.tuxfamily.org/tty-format/index.html")
